9b9c59bc986ffd44f54d133d4a1b5240d993fb98
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-02-19 12:02:19 +0530

    solve example of initialization

---
+++ assets/index.js/initialization.js
@@ -0,0 +1,8 @@
+// use let to initialization the value to name 
+let name = "Harsh";
+
+// use const to initialization the value to city 
+const city = "New York";
+
+// use var to initialization the value to country
+var country = "USA";
