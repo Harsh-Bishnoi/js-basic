d2ff3362fc268cf53dd22f64d61b079e93fc5b55
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-02-19 11:03:06 +0530

    solve example of scope

---
+++ assets/index.js/scope.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    let carName = "Volvo";
+    document.getElementById("1").innerHTML = carName + " car ";
+}
+
+document.getElementById("2").innerHTML = typeof carName;
+
+myFunction();
