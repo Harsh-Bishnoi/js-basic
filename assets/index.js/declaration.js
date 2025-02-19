7a539cf5fd5999cee90c0203e4bd56019cf6cae0
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-02-19 10:46:28 +0530

    solve declaration example

---
+++ assets/index.js/declaration.js
@@ -0,0 +1,17 @@
+// use function
+function study() {
+    console.log("12th class");
+}
+study();
+
+// use const
+const village = "Nadhori";
+console.log(village);
+
+// use let
+let age = 18;
+console.log(age);
+
+// use var
+var city = "Fatehabad";
+console.log(city);
