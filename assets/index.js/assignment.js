a94b500bef767e68a173b90c82a3d4b8630eb234
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-02-19 11:15:16 +0530

    solve example of assignment operator

---
+++ assets/index.js/assignment.js
@@ -0,0 +1,26 @@
+// Simple Assignment Operator
+let e = 10;
+console.log(e);
+
+// Addition Assignment Operator
+let f = 10;
+f += 990;
+console.log(f);
+
+
+// Subtraction Assignment Operator
+let g = 510;
+g -= 10;
+console.log(g);
+
+
+// Multiplication Assignment Operator
+let h = 200;
+h *= 2;
+console.log(h);
+
+
+// Division Assignment Operator
+let i = 200;
+i /= 2;
+console.log(i)
