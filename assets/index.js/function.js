62b412ed8891fd083dda0d819ead95782c811e2a
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-02-19 10:22:48 +0530

    solve example of function

---
+++ assets/index.js/function.js
@@ -0,0 +1,16 @@
+function myFunction(a, b) {
+    return a + b;
+}
+
+let x = myFunction(5, 74);
+
+console.log(x)
+
+
+function para(c, d) {
+    return c / d;
+}
+
+let y = para(30, 5);
+
+console.log(y)
