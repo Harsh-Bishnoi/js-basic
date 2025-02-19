88577328645b0aea13070366aca44cc802078822
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-02-19 10:23:14 +0530

    solve example of object

---
+++ assets/index.js/object.js
@@ -0,0 +1,16 @@
+let data = {
+    name: "Harsh",
+    lastName: "Bishnoi",
+    Age: "18",
+};
+
+console.log(data.lastName);
+
+const cars = {
+    carName: "Defender",
+    price: "2 crore",
+    model: "2024",
+    company:"Land Rover"
+}
+
+console.log(cars.carName);
